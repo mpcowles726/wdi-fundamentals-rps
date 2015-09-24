4cdd044b3d9d21e5b3928379d7e28b83cacd1484
Matthew Cowles <mpcowles726@gmail.com>
2015-09-24 14:16:24 -0700

    added the code for how to win rps

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,6 +40,22 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+         if (playerMove === 'rock' && computerMove === 'scissors') {
+            winner = 'player';
+         }
+          else if (playerMove === 'scissors' && computerMove === 'paper') {
+            winner = 'player';
+          }
+          else if (playerMove === 'paper' && computerMove === 'rock') {
+            winner = 'player';
+          }
+          else if (playerMove === computerMove) {
+            winner = 'tie';
+          }
+          else {
+            winner = 'computer';
+          }
+
     return winner; 
 }
 
